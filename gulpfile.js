7ea009da4c1cc3fe2683608e6c91538a468350b9
Kumaresan <36756050+kumaresan-subramani@users.noreply.github.com>
2022-03-07 10:58:40 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp');
 /** 
  * Compile TypeScript to JS
  */
-gulp.task('compile', gulp.series(function (done) {
+gulp.task('compile', gulp.series(function(done) {
     var ts = require('gulp-typescript');
     // Default typescript config
     var defaultConfig = {
@@ -18,10 +18,10 @@ gulp.task('compile', gulp.series(function (done) {
     tsResult = gulp.src(['./src/**/*.ts'], { base: '.' })
         .pipe(ts(tsProject))
         .pipe(gulp.dest('./'))
-        .on('error', function (e) {
+        .on('error', function(e) {
             done(e);
             process.exit(1);
-        }).on('end', function () {
+        }).on('end', function() {
             done();
         });
 }));
@@ -29,7 +29,7 @@ gulp.task('compile', gulp.series(function (done) {
 /**
  * Load the sample in src/app/index
  */
-gulp.task('start', gulp.series('compile', function (done) {
+gulp.task('start', gulp.series('compile', function(done) {
     var browserSync = require('browser-sync');
     var bs = browserSync.create('Essential JS 2');
     var options = {
@@ -51,48 +51,3 @@ gulp.task('start', gulp.series('compile', function (done) {
 function reportChanges(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
-/**
- * Testing spec files
- */
-var protractor = require('gulp-protractor').protractor;
-var webdriver_standalone = require('gulp-protractor').webdriver_standalone;
-var webdriver_update = require('gulp-protractor').webdriver_update_specific;
-
-gulp.task('e2e-serve', webdriver_standalone);
-
-gulp.task('e2e-webdriver-update', webdriver_update({
-    webdriverManagerArgs: ['--ie', '--edge']
-}));
-
-gulp.task('e2e-test', gulp.series('compile', function (done) {
-    var browserSync = require('browser-sync');
-    var bs = browserSync.create('Essential JS 2');
-    var options = {
-        server: {
-            baseDir: [
-                './src/app/',
-                './src/resource/',
-                './node_modules/@syncfusion/ej2/'
-            ],
-            directory: true
-        },
-        ui: false,
-        open: false,
-        notify: false
-    };
-    bs.init(options, function () {
-        gulp.src(['./spec/**/*.spec.js'])
-            .pipe(protractor({
-                configFile: 'e2e/protractor.conf.js'
-            }))
-            .on('error', function (e) {
-                console.error('Error: ' + e.message);
-                done();
-                process.exit(1);
-            })
-            .on('end', function () {
-                done();
-                process.exit(0);
-            });
-    });
-}));
